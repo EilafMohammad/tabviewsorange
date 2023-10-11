f48b85ddb24991a8fb4d4f72cea896a4ae066eaa
EilafMohammad <elafmohammadcs@gmail.com>
2023-10-11 10:19:04 +0300

    Initial Commit

---
+++ try1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  try1
+//
+//  Created by Eilaf Mohammad on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ try1/try1App.swift
@@ -0,0 +1,17 @@
+//
+//  try1App.swift
+//  try1
+//
+//  Created by Eilaf Mohammad on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct try1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
